4c5cd8901e9c7460e2308e0d5d9eb37d4fedf24c
ghost160111 <jalol100.ghost@yandex.com>
2023-07-10 09:13:06 +0500

    testing images linkage

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -2,11 +2,12 @@
   <article class="home">
     <section class="home-intro">
       <button class="g-btn">Hello</button>
-      <input class="g-input" type="text" />
+      <input class="g-input" type="text" v-model="inputText" :placeholder="inputText" />
+      <p>{{ inputText }}</p>
     </section>
     <div class="home-info">
       <ul class="home-info-list">
-        <li v-for="(card, i) in home.list" :key="i">
+        <!--<li v-for="(card, i) in home.list" :key="i">
           <div class="list-block">
             <div class="list-top">
               <img class="list-img" :src="card['image-link']" :alt="card.label" />
@@ -17,7 +18,22 @@
                 <img :src="card.arrow" />
               </div>
             </div>
-            <!--<a class="list-link" href="#"></a>-->
+            <a class="list-link" href="#"></a>
+          </div>
+        </li>-->
+        <li v-for="(card, i) in list"
+            :key="i">
+          <div class="list-block">
+            <div class="list-top">
+              <img class="list-img" :src="card['image-link']" :alt="card['label']" />
+            </div>
+            <div class="list-bottom">
+              <label class="list-label">{{ card['label'] }}</label>
+              <div class="list-arrow">
+                <img :src="card['arrow']" />
+              </div>
+            </div>
+            <a class="list-link" href="#"></a>
           </div>
         </li>
       </ul>
@@ -26,12 +42,32 @@
 </template>
 
 <script>
-import home from '../../assets/data/pages/home.json'
+
 
 export default {
   data() {
     return {
-      home
+      inputText: 'default input text',
+      list: [
+        {
+          "image-link": "./src/assets/other/images/icons/ico.svg",
+          "label": "Наличный и безналичный расчет"
+        },
+        {
+          "image-link": "./src/assets/other/images/icons/ico2.svg",
+          "label": "Техническая помощь и консультация"
+        },
+        {
+          "image-link": "./src/assets/other/images/icons/ico3.svg",
+          "label": "Только качественная и проверенная продукция",
+          "arrow": "./src/assets/other/images/icons/Union.svg"
+        },
+        {
+          "image-link": "./src/assets/other/images/icons/ico4.svg",
+          "label": "Мы всегда на связи с 9:00 до 18:00!",
+          "arrow": "./src/assets/other/images/icons/Union.svg"
+        }
+      ]
     }
   }
 }
